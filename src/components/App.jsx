da3c01929154e0b864292d9429c39284059f9786
Anastasiia-Ihn <128808242+Anastasiia-Ihn@users.noreply.github.com>
2023-10-01 15:41:11 +0200

    added 4-th task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,14 @@
-import { ProfileUser } from "./Profile/profile";
+import { ProfileUser } from './Profile/profile';
 import { Statistics } from './Statistics/statistics';
 import { FriendList } from './FriendList/friendList';
+import { TransactionHistory } from './TransactionHistory/transactionHistory';
 
 import user from 'user.json';
 import data from 'data.json';
 import friends from 'friends.json';
+import transactions from 'transactions.json';
+
+
 
 export const App = () => {
   return <>
@@ -16,8 +20,11 @@ export const App = () => {
       stats={user.stats} />
     
     <Statistics title="Upload stats" stats={data} />
-    <Statistics stats={data} />  
+    <Statistics stats={data} /> 
+    
     <FriendList friends={friends} />
+
+    <TransactionHistory items={transactions} />
 
   </>
 };
